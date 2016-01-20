d41b6100797ea1f85b6720a69c9f6c07a8ffdd80
unknown <10578602@CS514-09.ad.uvu.edu>
2016-01-20 09:51:15 -0700

    Script and whatnot

---
+++ Bioshock/New Unity Project/Assets/Script1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Firstscript : MonoBehaviour 
+{
+	public int myInt = 20;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		myInt = MultiplyByTwo (myInt);
+		Debug.Log (myInt);
+	}
+	int AddOne (int Number)
+	{
+		int ret;
+		ret = Number + 2;
+		return ret;
+		
+		
+	}
+}
